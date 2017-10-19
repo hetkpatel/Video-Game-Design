b42964068705cea3854633fad4042fb47aaaf9dc
Het <hp145371@gmail.com>
2017-10-19 10:10:56 -0500

    Add Torpedo Motion

--- The Borg/The Borg/The_Borg/Game1.cs
+++ The Borg/The Borg/The_Borg/Game1.cs
@@ -19,19 +19,24 @@ namespace The_Borg
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
-        Texture2D[] gunTurrets = new Texture2D[4];
-        Texture2D[] torpedos = new Texture2D[4];
-        Rectangle[] gunTurRcts = new Rectangle[4];
-        Rectangle[] torpedosRct = new Rectangle[4];
+        Texture2D[] gunTurrets = new Texture2D[4], torpedos = new Texture2D[4];
+        Texture2D borg;
+        Rectangle[] gunTurRcts = new Rectangle[4], torpedosRct = new Rectangle[4];
+        Rectangle borgRct;
         SpriteFont lsuFont;
 
         KeyboardState oldKb = Keyboard.GetState();
+        Keys[] numPad = {Keys.NumPad0, Keys.NumPad1, Keys.NumPad2, Keys.NumPad3,
+                        Keys.NumPad4, Keys.NumPad5, Keys.NumPad6,
+                        Keys.NumPad7, Keys.NumPad8, Keys.NumPad9};
 
         int delay = -1;
+        int time = 0;
 
-        int lsuEnergy = 100;
+        int lsuEnergy = 100, mjChoice = 0;
         int index = 0, shootingIndex = -1;
         int torpedoX = 0, torpedoY = 0;
+        
 
         public Game1()
         {
@@ -59,6 +64,8 @@ namespace The_Borg
             torpedosRct[2] = new Rectangle(375, 270, 25, 100);
             torpedosRct[3] = new Rectangle(250, 225, 100, 25);
 
+            borgRct = new Rectangle(0, 0, 100, 100);
+
             base.Initialize();
         }
 
@@ -81,6 +88,8 @@ namespace The_Borg
             torpedos[1] = this.Content.Load<Texture2D>("torpedo_right");
             torpedos[2] = this.Content.Load<Texture2D>("torpedo_down");
             torpedos[3] = this.Content.Load<Texture2D>("torpedo_left");
+
+            borg = this.Content.Load<Texture2D>("borg");
 
             lsuFont = this.Content.Load<SpriteFont>("LSUFont");
         }
@@ -105,6 +114,7 @@ namespace The_Borg
             // Allows the game to exit
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 this.Exit();
+            time += 1;
 
             // TODO: Add your update logic here
             if (kb.IsKeyDown(Keys.Up) &&
@@ -120,36 +130,79 @@ namespace The_Borg
                 !oldKb.IsKeyDown(Keys.Left))
                 index = 3;
 
+            for (int i = 0; i < numPad.Length;i++)
+            {
+                if (kb.IsKeyDown(numPad[i]))
+                {
+                    mjChoice = i;
+                }
+            }
+
             // TODO: Add torpedo motion
             if (kb.IsKeyDown(Keys.Space) &&
                 !oldKb.IsKeyDown(Keys.Space) && delay == -1)
             {
-                //switch (index)
-                //{
-                //    case 0:
-                //        // UP MOTION
-                //        break;
-                //    case 1:
-                //        // RIGHT MOTION
-                //        break;
-                //    case 2:
-                //        // DOWN MOTION
-                //        break;
-                //    case 3:
-                //        // LEFT MOTION
-                //        break;
-                //}
+                switch (index)
+                {
+                    case 0:
+                        torpedoX = 375;
+                        torpedoY = 100;
+                        break;
+                    case 1:
+                        torpedoX = 420;
+                        torpedoY = 225;
+                        break;
+                    case 2:
+                        torpedoX = 375;
+                        torpedoY = 270;
+                        break;
+                    case 3:
+                        torpedoX = 250;
+                        torpedoY = 225;
+                        break;
+                }
                 shootingIndex = index;
-
                 delay = gameTime.TotalGameTime.Seconds;
+                lsuEnergy -= mjChoice;
             }
 
-            if (delay != -1 &&
-                (gameTime.TotalGameTime.Seconds - delay) == 3)
+            if (delay != -1)
             {
-                delay = -1;
-                shootingIndex = -1;
+                if ((gameTime.TotalGameTime.Seconds - delay) < 3)
+                {
+                    switch (shootingIndex)
+                    {
+                        case 0:
+                            torpedoY -= 5;
+                            torpedosRct[shootingIndex] = new Rectangle(torpedoX, torpedoY, 25, 100);
+                            break;
+                        case 1:
+                            torpedoX += 5;
+                            torpedosRct[shootingIndex] = new Rectangle(torpedoX, torpedoY, 100, 25);
+                            break;
+                        case 2:
+                            torpedoY += 5;
+                            torpedosRct[shootingIndex] = new Rectangle(torpedoX, torpedoY, 25, 100);
+                            break;
+                        case 3:
+                            torpedoX -= 5;
+                            torpedosRct[shootingIndex] = new Rectangle(torpedoX, torpedoY, 100, 25);
+                            break;
+                    }
+                }
+                else
+                {
+                    delay = shootingIndex = -1;
+                    torpedoX = torpedoY = 0;
+                }
             }
+
+            if (lsuEnergy < 100 && (time % 60) == 0)
+            {
+                lsuEnergy += 3;
+            }
+            else if (lsuEnergy > 100)
+                lsuEnergy = 100;
                 
 
             oldKb = kb;
@@ -169,11 +222,12 @@ namespace The_Borg
                 spriteBatch.Draw(torpedos[shootingIndex], torpedosRct[shootingIndex], Color.White);
 
             for (int i = 0; i < 4; i++) {
-                if (i == index)
+                if (i == index && shootingIndex == -1)
                     spriteBatch.Draw(gunTurrets[i], gunTurRcts[i], Color.LightGreen);
                 else
                     spriteBatch.Draw(gunTurrets[i], gunTurRcts[i], Color.Red);
             }
+
             spriteBatch.DrawString(lsuFont, lsuEnergy+"MJ", new Vector2(360, 220), Color.White);
 
             spriteBatch.End();
