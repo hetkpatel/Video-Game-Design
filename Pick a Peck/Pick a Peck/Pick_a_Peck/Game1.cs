484a8db3a8758ed9cdcfd067d7474509e9e53440
Het <hp145371@gmail.com>
2017-11-05 16:27:35 -0600

    Work on Pick a Peck

--- Pick a Peck/Pick a Peck/Pick_a_Peck/Game1.cs
+++ Pick a Peck/Pick a Peck/Pick_a_Peck/Game1.cs
@@ -20,9 +20,11 @@ namespace Pick_a_Peck
         SpriteBatch spriteBatch;
 
         Texture2D sprite;
-        Rectangle sprRct;
 
         SpriteFont font;
+
+        bool showSelected = false;
+        int selectedBox = -1;
 
         public Game1()
         {
@@ -39,6 +41,7 @@ namespace Pick_a_Peck
         /// </summary>
         protected override void Initialize()
         {
+            this.IsMouseVisible = true;
             // TODO: Add your initialization logic here
 
             base.Initialize();
@@ -74,11 +77,44 @@ namespace Pick_a_Peck
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
+            KeyboardState kb = Keyboard.GetState();
             // Allows the game to exit
             if (Keyboard.GetState().IsKeyDown(Keys.Escape))
-                this.Exit();
+                    this.Exit();
 
             // TODO: Add your update logic here
+            if (!showSelected)
+            {
+                if (kb.IsKeyDown(Keys.D1) || kb.IsKeyDown(Keys.NumPad1))
+                {
+                    selectedBox = 1;
+                    showSelected = true;
+                }
+                else if (kb.IsKeyDown(Keys.D2) || kb.IsKeyDown(Keys.NumPad2))
+                {
+                    selectedBox = 2;
+                    showSelected = true;
+                }
+                else if (kb.IsKeyDown(Keys.D3) || kb.IsKeyDown(Keys.NumPad3))
+                {
+                    selectedBox = 3;
+                    showSelected = true;
+                }
+                else if (kb.IsKeyDown(Keys.D4) || kb.IsKeyDown(Keys.NumPad4))
+                {
+                    selectedBox = 4;
+                    showSelected = true;
+                }
+                else if (kb.IsKeyDown(Keys.D5) || kb.IsKeyDown(Keys.NumPad5))
+                {
+                    selectedBox = 5;
+                    showSelected = true;
+                }
+            } else if (kb.IsKeyDown(Keys.Back))
+            {
+                showSelected = false;
+                selectedBox = -1;
+            }
 
             base.Update(gameTime);
         }
@@ -92,31 +128,73 @@ namespace Pick_a_Peck
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
             // TODO: Add your drawing code here
-            // Rct 1
-            sprRct = new Rectangle(20, 20, 50, 50);
-            Rectangle imgRct = new Rectangle(278, 0, 50, 50);
-            spriteBatch.Draw(sprite, sprRct, imgRct, Color.White);
-            spriteBatch.DrawString(font, "1", new Vector2(35, 30), Color.White);
+            if (!showSelected)
+            {
+                int textY = 130;
+                // Rct 1
+                Rectangle sprRct = new Rectangle(20, 20, 50, 50);
+                Rectangle imgRct = new Rectangle(278, 0, 50, 50);
+                spriteBatch.Draw(sprite, sprRct, imgRct, Color.White);
+                spriteBatch.DrawString(font, "1", new Vector2(40, textY), Color.Black);
 
-            // Rct 2
-            sprRct = new Rectangle(100, 20, 150, 50);
-            imgRct = new Rectangle(0, 201, 150, 50);
-            spriteBatch.Draw(sprite, sprRct, imgRct, Color.White);
+                // Rct 2
+                sprRct = new Rectangle(100, 20, 150, 50);
+                imgRct = new Rectangle(0, 201, 150, 50);
+                spriteBatch.Draw(sprite, sprRct, imgRct, Color.White);
+                spriteBatch.DrawString(font, "2", new Vector2(170, textY), Color.Black);
 
-            // Rct 3
-            sprRct = new Rectangle(300, 20, 50, 150);
-            imgRct = new Rectangle(227, 0, 50, 150);
-            spriteBatch.Draw(sprite, sprRct, imgRct, Color.White);
+                // Rct 3
+                sprRct = new Rectangle(300, 20, 25, 75);
+                imgRct = new Rectangle(227, 0, 50, 150);
+                spriteBatch.Draw(sprite, sprRct, imgRct, Color.White);
+                spriteBatch.DrawString(font, "3", new Vector2(307, textY), Color.Black);
 
-            // Rct 4
-            sprRct = new Rectangle(400, 20, 75, 200);
-            imgRct = new Rectangle(151, 0, 75, 200);
-            spriteBatch.Draw(sprite, sprRct, imgRct, Color.White);
+                // Rct 4
+                sprRct = new Rectangle(400, 20, 38, 100);
+                imgRct = new Rectangle(151, 0, 75, 200);
+                spriteBatch.Draw(sprite, sprRct, imgRct, Color.White);
+                spriteBatch.DrawString(font, "4", new Vector2(414, textY), Color.Black);
 
-            // Rct 5
-            sprRct = new Rectangle(550, 20, 150, 200);
-            imgRct = new Rectangle(0, 0, 150, 200);
-            spriteBatch.Draw(sprite, sprRct, imgRct, Color.White);
+                // Rct 5
+                sprRct = new Rectangle(550, 20, 75, 100);
+                imgRct = new Rectangle(0, 0, 150, 200);
+                spriteBatch.Draw(sprite, sprRct, imgRct, Color.White);
+                spriteBatch.DrawString(font, "5", new Vector2(582, textY), Color.Black);
+            } else
+            {
+                Rectangle sprRct = new Rectangle();
+                Rectangle imgRct = new Rectangle();
+                switch (selectedBox)
+                {
+                    case 1:
+                        sprRct = new Rectangle(GraphicsDevice.Viewport.Width/2 - 50, GraphicsDevice.Viewport.Height/2 - 50, 100, 100);
+                        imgRct = new Rectangle(278, 0, 50, 50);
+                        spriteBatch.Draw(sprite, sprRct, imgRct, Color.White);
+                        break;
+                    case 2:
+                        sprRct = new Rectangle(GraphicsDevice.Viewport.Width/2 - 150, GraphicsDevice.Viewport.Height/2 - 50, 300, 100);
+                        imgRct = new Rectangle(0, 201, 150, 50);
+                        spriteBatch.Draw(sprite, sprRct, imgRct, Color.White);
+                        break;
+                    case 3:
+                        sprRct = new Rectangle(GraphicsDevice.Viewport.Width/2 - 25, GraphicsDevice.Viewport.Height/2 - 75, 50, 150);
+                        imgRct = new Rectangle(227, 0, 50, 150);
+                        spriteBatch.Draw(sprite, sprRct, imgRct, Color.White);
+                        break;
+                    case 4:
+                        sprRct = new Rectangle(GraphicsDevice.Viewport.Width/2 - 38, GraphicsDevice.Viewport.Height/2 - 100, 75, 200);
+                        imgRct = new Rectangle(151, 0, 75, 200);
+                        spriteBatch.Draw(sprite, sprRct, imgRct, Color.White);
+                        break;
+                    case 5:
+                        sprRct = new Rectangle(GraphicsDevice.Viewport.Width/2 - 75, GraphicsDevice.Viewport.Height/2- 100, 150, 200);
+                        imgRct = new Rectangle(0, 0, 150, 200);
+                        spriteBatch.Draw(sprite, sprRct, imgRct, Color.White);
+                        break;
+                }
+                spriteBatch.DrawString(font, "Press BACKSPACE to go back.", new Vector2(250, 400), Color.Black);
+            }
+
 
             spriteBatch.End();
             base.Draw(gameTime);
