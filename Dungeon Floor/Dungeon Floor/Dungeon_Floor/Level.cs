b4bb964d503b74f0e03e542f627b7fe39d9253b4
Het <hp145371@gmail.com>
2018-04-02 10:14:42 -0500

    Add Dungeon Floor

---
+++ Dungeon Floor/Dungeon Floor/Dungeon_Floor/Level.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using System.IO;
+
+namespace Dungeon_Floor
+{
+    class Level : IDisposable
+    {
+        private Tile[,] tiles;
+        private Dictionary<string, Texture2D> tileSheets;
+        public Dictionary<int, Rectangle> TileSourceRects;
+        public ContentManager Content
+        {
+            get { return content; }
+        }
+        ContentManager content;
+
+
+
+        public void Dispose()
+        {
+            Content.Unload();
+        }
+    }
+}
